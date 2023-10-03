78bf2b94bf9c5d0530520ff83938ee720c22440f
evelyn masso <coding@outofambit.com>
2023-10-03 14:26:54 -0700

    even more minimal example

--- index.ts
+++ index.ts
@@ -3,23 +3,10 @@ import puppeteer from "puppeteer";
 const run = async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    devtools: true,
-    slowMo: 500,
-    debuggingPort: 9222,
   });
-
   const mainPage = await browser.newPage();
-  mainPage.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
-  await mainPage.goto(
-    "http://googlechrome.github.io/samples/web-bluetooth/get-devices-async-await.html"
-  );
-  await mainPage.waitForNetworkIdle();
-  const [devicePrompt] = await Promise.all([
-    mainPage.waitForDevicePrompt(),
-    mainPage.locator("#requestBluetoothDevice").click(),
-  ]);
-
-  console.log(devicePrompt.devices);
+  await mainPage.goto("http://google.com");
+  await mainPage.waitForDevicePrompt();
 };
 
 run();
