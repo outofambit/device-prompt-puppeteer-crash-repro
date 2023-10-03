d4d73603184797017c4009ecd9a6f9bac89afd6d
evelyn masso <coding@outofambit.com>
2023-10-03 11:44:31 -0700

    use other website to test

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
 import puppeteer from "puppeteer";
 
 const run = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    devtools: true,
+    slowMo: 500,
+    debuggingPort: 9222,
+  });
+
   const mainPage = await browser.newPage();
-  await mainPage.goto("http://espruino.com/ide");
+  mainPage.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+  await mainPage.goto(
+    "http://googlechrome.github.io/samples/web-bluetooth/get-devices-async-await.html"
+  );
+  await mainPage.waitForNetworkIdle();
   const [devicePrompt] = await Promise.all([
     mainPage.waitForDevicePrompt(),
     mainPage.locator("#requestBluetoothDevice").click(),
   ]);
+
   console.log(devicePrompt.devices);
 };
 
